596efe8eccbe5ff4ebf79dea22e437c47bba1997
Fabro0 <47898082+Fabro0@users.noreply.github.com>
2020-07-01 18:45:03 -0300

    Update python.js

--- main/backend/routes/python.js
+++ main/backend/routes/python.js
@@ -8,50 +8,25 @@ router.route('/tool')
 	var largeDataSet = [];
     
 	const python = spawn('python', ['train-pero-bien.py','1a2b2b']);
-	
 	await python.stdout.on('data',  (data) => {
 		
 		largeDataSet.push(data);
 		res.json({message:largeDataSet.join("")})
 
 	});
-	
-	
 })
+router.route('/')
+.post(async (req,res) => {
 
-
-// router.route('/')
-// .get((req,res) => {
-//     const link_user = 'https://www.freeformatter.com/json-escape.html';
+    const link_user = req.body.link;
+    var dataToSend ;
+	var largeDataSet = [];
     
-//     var dataToSend ;
-// 	var largeDataSet = [];
-    
-// 	const python = spawn('python', ['script1.py',link_user]);
-	
-// 	python.stdout.on('data', function (data) {
-// 		largeDataSet.push(data);
-// 	});
-	
-// 	python.on('close', (code) => {
-//     res.json({message:largeDataSet.join("")})
-// 	});
-// })
-// .post(async (req,res) => {
-
-//     const link_user = req.body.link;
-//     var dataToSend ;
-// 	var largeDataSet = [];
-    
-// 	const python = spawn('python', ['script1.py',link_user]);
-	
-// 	await python.stdout.on('data',  (data) => {
-// 		largeDataSet.push(data);
-// 		res.json({message:largeDataSet.join("")})
-
-// 	});
-	
-	
-// })
+	const python = spawn('python', ['script1.py',link_user]);
+	await python.stdout.on('data',  (data) => {
+		largeDataSet.push(data);
+		res.json({message:largeDataSet.join("")})
+	});
+})
 
 module.exports = router
