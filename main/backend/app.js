2736642998d6645cc2465e67e226b4613bf090b4
Hollow9 <35386016+Hollow9@users.noreply.github.com>
2020-09-11 14:13:04 -0300

    mongo cloud no lo subi

--- main/backend/app.js
+++ main/backend/app.js
@@ -6,13 +6,15 @@ const cors = require('cors');
 const path = require('path');
 const dnname = 'testa'
 // const uri = "mmongodb+srv://root:rootroot@lurien.3ls4u.mongodb.net/lurien?retryWrites=true&w=majority";
+const uri = "mongodb+srv://tievo:hU4s1oAElAanEEHu@lurien.1yjjv.mongodb.net/tievo?retryWrites=true&w=majority";
+
 
 app.use(express.static(path.join(__dirname, 'client/build')))
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors());
-const uri = 'mongodb://localhost:27017/mernauth'
+//const uri = 'mongodb://localhost:27017/mernauth'
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, () => {
     console.log('successfully connected to database');
